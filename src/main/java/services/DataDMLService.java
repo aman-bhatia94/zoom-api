85c8efafe038a091e850c56c7f97eabf4cd5af90
Aman Bhatia <amanbhatia@Amans-MacBook-Pro.local>
2020-06-03 22:16:44 -0700

    refactored

--- src/main/java/services/DataDMLService.java
+++ src/main/java/services/DataDMLService.java
@@ -5,6 +5,9 @@ import services.data.DBResponseData;
 
 import java.lang.reflect.Field;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DataDMLService {
 
@@ -30,15 +33,42 @@ public class DataDMLService {
                 tableName = mainField.get(requestData).toString();
             }
         }
+        String finalSql = null;
+        StringBuilder sql = new StringBuilder();
+        sql.append( "SELECT * FROM "+tableName);
+        if(queryObj != null){
+            sql.append(" WHERE ");
+            Field[] fields = queryObj.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                String fieldName = field.getName();
+                Object val = field.get(queryObj);
+                sql.append(fieldName+ " = "+val.toString() + " AND ");
+                System.out.println("Name: " + fieldName + "\nval: " + val.toString());
+            }
+            String sqlToRun = sql.toString();
+            int lastIndexOfAnd = sqlToRun.lastIndexOf("AND");
+            StringBuilder temp = new StringBuilder();
+            temp.append(sqlToRun.substring(0,lastIndexOfAnd));
+            temp.append(";");
+            finalSql = temp.toString();
+        }
+        else{
+            sql.append(";");
+            finalSql = sql.toString();
+        }
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(finalSql);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
         System.out.println("Query: " + queryObj);
 
-        Field[] fields = queryObj.getClass().getDeclaredFields();
-        for (Field field : fields) {
-            String fieldName = field.getName();
-            Object val = field.get(queryObj);
-            System.out.println("Name: " + fieldName + "\nval: " + val.toString());
-        }
+
 
         return null;
     }
